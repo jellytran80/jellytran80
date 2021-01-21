02c758f2e2e8b0f81cc22437b33cfcd579e5a7da
Anh Tran <jellytran80@yahoo.com>
2021-01-21 11:48:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const getQuote = async () => {
     const { data } = await axios.get("https://quotes.rest/qod?language=en");
     const quote = data.contents.quotes[0].quote;
     const author = data.contents.quotes[0].author;
-
     // console.log("new quote", `"${quote}"`);
-
     return {
       quote,
       author,
@@ -21,9 +19,7 @@ const getQuote = async () => {
 
 const generate = async () => {
   const { quote, author } = await getQuote();
-
   if (!quote) return;
-
   fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
 };
 
